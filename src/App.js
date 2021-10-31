09ed86b975048a2b01c9b6b1641447abc2a87535
Thomas Xu <thomas.xu@dayonedigital.com.au>
2021-11-01 09:44:04 +1100

    react todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import {Button,TextField,FormControl,List,ListItem } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [todo, setTodo] = useState("");
+    const [todos,setTodos] =useState([])
+    const [editMode, setEditMode]=useState(false)
+    const [editValue, setEditValue] = useState("")
+    // const todos = ["Finish homework", "Wash dishes", "Clean room", "Make waffles"];
+    const handleAdd=()=>{
+       setTodos([...todos, todo])
+       setTodo("")
+        // console.log(todos)
+    }
+    const handleDelete=(item)=>{
+        
+        setTodos(todos.filter((e)=> e !== item))
+    }
+
+    const handleEdit=(item)=>{
+        console.log(item)
+        setEditValue(item)
+        setEditMode(!editMode)
+    }
+
+    const handleDone=(e)=>{
+        setEditMode(!editMode)
+        console.log(e)
+    }
+
+    return (
+        <>
+         <FormControl variant="standard">
+        <TextField id="standard-basic" label="To Dos" variant="standard" onChange={(e)=>setTodo(e.target.value)} value={todo}/>
+        
+        <Button variant="contained" color="primary" onClick={handleAdd}>
+            Add Tesk
+        </Button>
+        <List>
+            {todos.length===0 ? <listItem>There are no items.</listItem>: todos.map(task => (
+                editValue===task ?<>
+                <TextField key={task} placeholder={task} onChange={(e)=>setTodo(e.target.value)}/><Button onClick={()=>handleDelete(task)}> remove </Button> 
+                <Button onClick={()=>handleDone(todo)}> Done </Button></>
+                :<ListItem key={task}>  {task} <Button onClick={()=>handleDelete(task)}> remove </Button> 
+                <Button onClick={()=>handleEdit(task)}> Edit </Button></ListItem>
+            ))}
+        </List>
+        </FormControl>
+        </>
+    )
 }
-
+ 
 export default App;
